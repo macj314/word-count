dae43a73faa5bda5a78f78308665eaef60b2fec7
unknown <macjay314@gmail.com>
2020-05-15 14:17:22 -0700

    Create private static list _wordList. Add words from Sentence to this list in the constructor

--- WordCounter/Models/Counter.cs
+++ WordCounter/Models/Counter.cs
@@ -1,17 +1,22 @@
+using System.Collections.Generic;
+
 namespace WordCounter.Models
 {
   public class Counter
   {
     public string SearchTerm { get; set; }
     public string Sentence { get; set; }
+    private static List<string> _wordList = new List<string>{};
     public Counter(string searchterm, string sentence)
     {
       SearchTerm = searchterm;
       Sentence = sentence;
+      string[] stringArray = sentence.Split();
+      _wordList.AddRange(stringArray);
     }
     public string GetAll()
     {
-      return $"Your search word is: '{SearchTerm}' and your sentence is: '{Sentence}'";
+      return $"Your search word is: '{SearchTerm}' and your sentence is: '{Sentence}'" + _wordList;
     }
   }
 }
